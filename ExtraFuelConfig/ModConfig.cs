d0ffa41deb498b113313c8c7e6e7669ff1c955fb
zombifier <zombify.25@gmail.com>
2024-03-26 00:49:35 -0400

    Rename files and add update key

---
+++ ExtraFuelConfig/ModConfig.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+public sealed class FuelConfig {
+  public IList<string> extraFuel = new List<string>();
+}
+
+public sealed class ModConfig {
+  public IDictionary<String, FuelConfig> flavoredItems = new Dictionary<String, FuelConfig>() {
+    {"(O)812", new FuelConfig() }, // Roe
+    {"(O)447", new FuelConfig() }, // Aged Roe
+    {"(O)340", new FuelConfig() }, // Honey
+    {"(O)344", new FuelConfig() }, // Jelly
+    {"(O)350", new FuelConfig() }, // Juice
+    {"(O)342", new FuelConfig() }, // Pickle
+    {"(O)348", new FuelConfig() }, // Wine
+    {"(O)SpecificBait", new FuelConfig() },
+    {"(O)DriedFruit", new FuelConfig() },
+    {"(O)DriedMushrooms", new FuelConfig() },
+    {"(O)SmokedFish", new FuelConfig() },
+
+    // Atelier wildflour's milk mushrooms
+    {"(O)wildflour.ateliercrops_milkshroom", new FuelConfig {extraFuel = new List<string>() {"-6"}} },
+  };
+}
