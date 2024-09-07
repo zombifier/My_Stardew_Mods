8edb1c3a0c9d03492b87352726fdc82a6484548e
zombifier <zombify.25@gmail.com>
2024-09-07 02:01:38 -0400

    Upload fixed version of asset handler

--- Common/BaseAssetHandler.cs
+++ Common/BaseAssetHandler.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
-using StardewValley.GameData.Machines;
-using StardewValley.GameData.BigCraftables;
-using Microsoft.Xna.Framework.Graphics;
 
 using SObject = StardewValley.Object;
 
@@ -15,7 +12,16 @@ public abstract class DictAssetHandler<AssetType> {
   private string dataPath;
   private IMonitor monitor;
 
-  public Dictionary<string, AssetType> data;
+  private Dictionary<string, AssetType>? privateData = null;
+  public Dictionary<string, AssetType> data {
+    get {
+      if (privateData == null) {
+        privateData = Game1.content.Load<Dictionary<string, AssetType>>(this.dataPath);
+        monitor.Log($"Loaded asset {dataPath} with {data.Count} entries.");
+      }
+      return privateData!;
+    }
+  }
 
   public DictAssetHandler(string dataPath, IMonitor monitor) {
     this.dataPath = dataPath;
@@ -24,36 +30,22 @@ public abstract class DictAssetHandler<AssetType> {
 
   public void RegisterEvents(IModHelper helper) {
     helper.Events.Content.AssetRequested += this.OnAssetRequested;
-    helper.Events.Content.AssetReady += this.OnAssetReady;
     helper.Events.Content.AssetsInvalidated += this.OnAssetsInvalidated;
-    helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
   }
 
-  public void OnAssetRequested(object sender, AssetRequestedEventArgs e) {
+  public void OnAssetRequested(object? sender, AssetRequestedEventArgs e) {
     if (e.NameWithoutLocale.IsEquivalentTo(this.dataPath)) {
       var dict = new Dictionary<string, AssetType>();
       e.LoadFrom(() => dict, AssetLoadPriority.Low);
     }
   }
 
-  public void OnAssetReady(object sender, AssetReadyEventArgs e) {
-    if (e.NameWithoutLocale.IsEquivalentTo(this.dataPath)) {
-      this.data = Game1.content.Load<Dictionary<string, AssetType>>(this.dataPath);
-      monitor.Log($"Loaded asset {dataPath} with {data.Count} entries.");
-    }
-  }
-
-  public void OnGameLaunched(object sender, GameLaunchedEventArgs e) {
-    this.data = Game1.content.Load<Dictionary<string, AssetType>>(this.dataPath);
-  }
-
-  public void OnAssetsInvalidated(object sender, AssetsInvalidatedEventArgs e) {
+  public void OnAssetsInvalidated(object? sender, AssetsInvalidatedEventArgs e) {
     foreach (var name in e.NamesWithoutLocale) {
       if (name.IsEquivalentTo(this.dataPath)) {
         monitor.Log($"Asset {dataPath} invalidated, reloading.");
-        this.data = Game1.content.Load<Dictionary<string, AssetType>>(this.dataPath);
+        this.privateData = null;
       }
     }
   }
-
 }
