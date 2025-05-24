a2290bfdf131996a0a7cd6c60c1a7cadfc317917
zombifier <zombify.25@gmail.com>
2025-05-24 01:08:45 -0400

    Fix NRE caused by who being null

--- ExtraMachineConfig/MachineHarmonyPatcher.cs
+++ ExtraMachineConfig/MachineHarmonyPatcher.cs
@@ -617,12 +617,12 @@ sealed class MachineHarmonyPatcher {
   // reset to null immediately before collecting invalid messages
   private static string? invalidMessage = null;
 
-  public static void MachineDataUtility_CanApplyOutput_Postfix(ref bool __result, SObject machine, MachineOutputRule rule, MachineOutputTrigger trigger, Item inputItem, Farmer who, GameLocation location, ref MachineOutputTriggerRule? triggerRule, ref bool matchesExceptCount) {
+  public static void MachineDataUtility_CanApplyOutput_Postfix(ref bool __result, SObject machine, MachineOutputRule rule, MachineOutputTrigger trigger, Item inputItem, Farmer? who, GameLocation location, ref MachineOutputTriggerRule? triggerRule, ref bool matchesExceptCount) {
     // skip this output rule if all output items are missing fuels
     if (__result) {
       List<MachineItemOutput>? outputs = rule?.OutputItem;
       if (outputs != null && outputs.Count > 0) {
-        IInventory inventory = SObject.autoLoadFrom ?? who.Items;
+        IInventory inventory = SObject.autoLoadFrom ?? (who ?? Game1.player).Items;
         bool noneMatch = true;
         foreach (MachineItemOutput output in outputs) {
           if (output.CustomData != null) {
